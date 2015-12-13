ccdd1c289737de66d457a31264b332a23ea29932
suntinghui <tinghuisun@163.com>
2015-12-13 10:18:35 +0800

    STH

---
+++ build/generated/source/buildConfig/release/com/wufriends/gugu/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wufriends.gugu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.wufriends.gugu";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 223;
+  public static final String VERSION_NAME = "";
+}
---
+++ src/com/ares/baggugu/dto/app/HQInfoAppDto.java
@@ -0,0 +1,80 @@
+package com.ares.baggugu.dto.app;
+
+import java.io.Serializable;
+
+public class HQInfoAppDto implements Serializable {
+
+	private static final long serialVersionUID = 3312186903548162174L;
+	/**
+	 * 年收益率
+	 */
+	private String rate;
+	/**
+	 * 正在反息人数
+	 */
+	private int userCount;
+	/**
+	 * 可售金额
+	 */
+	private String surplusMoney;
+	/**
+	 * 总金额
+	 */
+	private String totalMoney;
+	/**
+	 * 超过银行多少倍
+	 */
+	private int bank;
+	/**
+	 * 活期可售或定期已完成
+	 */
+	private String surplusMoneyStr;
+
+	public String getRate() {
+		return rate;
+	}
+
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+
+	public int getUserCount() {
+		return userCount;
+	}
+
+	public void setUserCount(int userCount) {
+		this.userCount = userCount;
+	}
+
+	public String getSurplusMoney() {
+		return surplusMoney;
+	}
+
+	public void setSurplusMoney(String surplusMoney) {
+		this.surplusMoney = surplusMoney;
+	}
+
+	public String getTotalMoney() {
+		return totalMoney;
+	}
+
+	public void setTotalMoney(String totalMoney) {
+		this.totalMoney = totalMoney;
+	}
+
+	public int getBank() {
+		return bank;
+	}
+
+	public void setBank(int bank) {
+		this.bank = bank;
+	}
+
+	public String getSurplusMoneyStr() {
+		return surplusMoneyStr;
+	}
+
+	public void setSurplusMoneyStr(String surplusMoneyStr) {
+		this.surplusMoneyStr = surplusMoneyStr;
+	}
+}
